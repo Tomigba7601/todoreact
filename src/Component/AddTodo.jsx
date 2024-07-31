1a1a2de6546105c819d4dbbfcc409c013334a4c0
Agbooa12 <agboolaelijah077@gmail.com>
2024-07-31 10:47:29 +0100

    wip on laravel with react, adding todo is working

--- src/Component/AddTodo.jsx
+++ src/Component/AddTodo.jsx
@@ -1,9 +1,12 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router';
 
 const AddTodo = () => {
+    const navigate = useNavigate();
     const todo_name = useRef()
     const todo_desc = useRef()
+    const [message, setMessage] = useState()
 
     const submitHandle = (e) => {
         e.preventDefault();
@@ -12,17 +15,20 @@ const AddTodo = () => {
             todo_desc: todo_desc.current.value
         };
         const baseUrl = "http://127.0.0.1:8000/api/todo/create";
-        
-        axios.post(baseUrl , data).then(
-            res=>{
-                    console.log(res);
-                })
-                .catch(err=>{
-                    console.log(err);
-                })
-                
-                // todo_name.current.value = '';
-                // todo_desc.current.value = '';
+
+        axios.post(baseUrl, data).then(
+            res => {
+                if(res.data.status){
+                setMessage(res.data.message);
+                    navigate("/viewtodo");
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            })
+
+        todo_name.current.value = '';
+        todo_desc.current.value = '';
     }
 
     return (
@@ -30,7 +36,8 @@ const AddTodo = () => {
             <div className=" text-white p-8 rounded shadow w-full max-w-md">
 
                 <h2 className="text-3xl text-white text-center font-bold mb-6 ">Add Todo</h2>
-                <form  method="POST" className="space-y-4" >
+                <p>{message}</p>
+                <form method="POST" className="space-y-4" >
                     <div>
                         <label htmlFor="todo_name" className="block text-white text-xl font-bold">Todo_Name</label>
                         <input type="text" id="todo_name" name="todo_name" ref={todo_name} className="mt-1 text-black block w-full p-2 rounded-md shadow-sm" placeholder='Enter your todo_name' />
